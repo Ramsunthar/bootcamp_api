6932d67ac0347ff8e796e3841819e9fbfd023007
ezioguga <ezioguga@gmail.com>
2020-12-28 19:35:10 +0530

    added protect and authorize middleware

--- routes/courses.js
+++ routes/courses.js
@@ -11,6 +11,8 @@ const {
 
 const Course = require("../models/Course");
 
+const { protect, authorize } = require("../middleware/auth");
+
 const advancedResults = require("../middleware/advancedResults");
 
 router
@@ -22,7 +24,11 @@ router
     }),
     getCourses
   )
-  .post(addCourse);
-router.route("/:id").get(getCourse).put(updateCourse).delete(deleteCourse);
+  .post(protect, authorize("publisher", "admin"), addCourse);
+router
+  .route("/:id")
+  .get(getCourse)
+  .put(protect, authorize("publisher", "admin"), updateCourse)
+  .delete(protect, authorize("publisher", "admin"), deleteCourse);
 
 module.exports = router;
